062150853bff0f0279cd6ba5e4619d6b9273cdda
yusufkaramustafa <ykaramustafa00@gmail.com>
2025-11-24 15:01:45 +0300

    Preprocessing of log files with templates

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_extract, col, when, lit, regexp_replace
+from functools import reduce
 
 spark = SparkSession.builder \
     .appName("HDFS_Anomaly_Detection") \
@@ -10,8 +12,45 @@ spark = SparkSession.builder \
 print("Spark session created!")
 
 log_path = "data/HDFS.log"
+template_path = "data/HDFS_templates.csv"
 
 df_logs = spark.read.text(log_path)
+df_templates = spark.read.csv(template_path, header=True)
 
-df_logs.show(20, truncate=False)
-df_logs.printSchema()
+# Convert templates into regex patterns
+df_templates = df_templates.withColumn(
+    "Regex",
+    regexp_replace(col("EventTemplate"), "<\\*>", "(.*)")
+)
+
+# Start with EventId = null
+df_parsed = df_logs.withColumn("EventId", col("value"))
+
+# Apply templates
+for row in df_templates.collect():
+    event_id = row["EventId"]
+    pattern = row["Regex"]
+
+    df_parsed = df_parsed.withColumn(
+        "EventId",
+        when(col("value").rlike(pattern), event_id).otherwise(col("EventId"))
+    )
+
+for row in df_templates.collect():
+    eid = row["EventId"]
+    pattern = row["Regex"]
+
+    df_parsed = df_parsed.withColumn(
+        "Parameter1",
+        when(col("EventId") == eid, regexp_extract(col("value"), pattern, 1))
+    ).withColumn(
+        "Parameter2",
+        when(col("EventId") == eid, regexp_extract(col("value"), pattern, 2))
+    ).withColumn(
+        "Parameter3",
+        when(col("EventId") == eid, regexp_extract(col("value"), pattern, 3))
+    )
+
+
+df_parsed.show(20, truncate=False)
+df_parsed.printSchema()
